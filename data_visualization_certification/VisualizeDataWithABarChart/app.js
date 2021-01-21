751afa5b9ad9353235ac411e29b7183339ba71f2
raiesbo <raiesbo8@gmail.com>
2021-01-21 15:28:52 +0100

    fetched data, layout, diagram frist steps

--- data_visualization_certification/VisualizeDataWithABarChart/app.js
+++ data_visualization_certification/VisualizeDataWithABarChart/app.js
@@ -1,7 +1,40 @@
 
-let data;
-const url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json"
 
-fetch(url)
-    .then(response => response.json)
-    .then(fetchedData => data = fetchedData)
+const url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
+
+
+const chart = (chartData) => {
+
+    console.log("chartData", chartData.data.map(d => d[1]))
+
+    const w = "60vw";
+    const h = "60vh";
+    const margin = {
+        top: 100,
+        right: 50,
+        bottom: 50,
+        left: 50
+    }
+
+
+    const yAxis = d3.scaleLinear()
+    // .domian(0, d3.max(chartData.data, (d) => d[1]))
+    // .range()
+
+    const svg = d3.select(".main")
+        .append("svg")
+        .attr("width", w)
+        .attr("height", h)
+    // .style("background-color", "red")
+}
+
+
+
+const fetchedData = d3.json(url)
+    .then(d => {
+        console.log(d.data)
+        return chart(d)
+    })
+    .catch(e => console.log(e))
+
+console.log("fetchedData", fetchedData)
