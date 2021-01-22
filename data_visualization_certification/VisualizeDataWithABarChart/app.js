c15d3ec7de9cec93d0562f4eea4807706716d103
raiesbo <raiesbo8@gmail.com>
2021-01-22 10:29:34 +0100

    visual data, missing axles

--- data_visualization_certification/VisualizeDataWithABarChart/app.js
+++ data_visualization_certification/VisualizeDataWithABarChart/app.js
@@ -4,28 +4,59 @@ const url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData
 
 
 const chart = (chartData) => {
-
     console.log("chartData", chartData.data.map(d => d[1]))
-
-    const w = "60vw";
-    const h = "60vh";
-    const margin = {
-        top: 100,
+    ///////////////////////
+    // CANVAS DIMENTIONS //
+    ///////////////////////
+    const w = 800;
+    const h = 450;
+    const padding = {
+        top: 50,
         right: 50,
-        bottom: 50,
+        bottom: 100,
         left: 50
     }
 
+    const xScale = d3.scaleBand()
+        .domain(chartData.data.map(d => d[0]))
+        .range([padding.left, w - padding.right])
+        .padding(0.1)
+    const yScale = d3.scaleLinear()
+        .domain([0, d3.max(chartData.data, (d) => d[1])])
+        .range([h - padding.top, padding.bottom])
 
-    const yAxis = d3.scaleLinear()
-    // .domian(0, d3.max(chartData.data, (d) => d[1]))
-    // .range()
+    console.log(xScale)
+    
 
     const svg = d3.select(".main")
         .append("svg")
         .attr("width", w)
         .attr("height", h)
     // .style("background-color", "red")
+
+
+    const bars = svg.selectAll("rect")
+        .data(chartData.data)
+        .enter()
+        .append("rect")
+        .attr("width", xScale.bandwidth())
+        .attr("height", d => h - yScale(d[1]))
+        .attr("x", d => xScale(d[0]))
+        .attr("y", d => yScale(d[1]))
+    
+    const xAxis = d3.axisBottom(xScale)
+    const yAxis = d3.axisLeft(yScale)
+
+    svg.append("g")
+        .attr(`transform, translate(0, ${h - padding.left})`)
+        .call(xAxis)
+
+    svg.append("g")
+        .attr(`transform, translate(${padding.left}, 0)`)
+        .call(yAxis)
+
+
+
 }
 
 
